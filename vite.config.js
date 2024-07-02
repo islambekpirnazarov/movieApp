f55f7fefd721c48ba1ba97b2a525826cb7863cb7
islambekpirnazarov <islampirnazarov82@gmail.com>
2024-07-02 11:05:41 +0300

    moives

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        search: resolve(__dirname, 'search/search.html'),
+        tvshows: resolve(__dirname, 'tvshows/tvshows.html'),
+        categories: resolve(__dirname, 'categories/categories.html'),
+        detail: resolve(__dirname, 'detail/detail.html'),
+      },
+    },
+  },
+})
